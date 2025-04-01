e50aab8eb2fd43a8a0b9b8ef96f0c084d241fc2d
FabienThich <fabienthich30@gmail.com>
2025-04-01 18:03:29 -0400

    add classes

--- data_analysis.py
+++ data_analysis.py
@@ -1,11 +1,17 @@
 from data_loader import BUDGET_2020, BUDGET_2021, BUDGET_2022, BUDGET_2023, BUDGET_2024, BUDGET_2025
+from typing import List
+import doctest
 
 class Entry:
-    def __init__(self, id, year, budget_type, organization, command_name, pillar_name, district_name, unit_name, feature_category, cost_estimate, long_name, cost_actual):
+    def __init__(self, data: str):
 
         """
         An entry for a row in the toronto police budget dataset.
         We are mainly interested in all the elements listed below except for _id, _organization, _command_name, _pillar_name, and _district_name
+
+        >>> my_entry = Entry(BUDGET_2020[2])
+        >>> print(my_entry)
+        Entry(2, 2020, Approved Budget, 1 - Toronto Police Service, Centralized Service Charges, Centralized Service Charges, Centralized Service Charges, CCC - Central Paid Duty, Revenues, 8502, PAID DUTY- OFFICERS FEE, -24667000)
         """
 
         _id : int
@@ -21,18 +27,18 @@ class Entry:
         _long_name : str
         _cost_actual : float
 
-        self._id = id
-        self._year = year
-        self._budget_type = budget_type
-        self._organization = organization
-        self._command_name = command_name
-        self._pillar_name = pillar_name
-        self._district_name = district_name
-        self._unit_name = unit_name
-        self._feature_category = feature_category
-        self._cost_estimate = cost_estimate
-        self._long_name = long_name
-        self._cost_actual = cost_actual
+        self._id = data[0]
+        self._year = data[1]
+        self._budget_type = data[2]
+        self._organization = data[3]
+        self._command_name = data[4]
+        self._pillar_name = data[5]
+        self._district_name = data[6]
+        self._unit_name = data[7]
+        self._feature_category = data[8]
+        self._cost_estimate = data[9]
+        self._long_name = data[10]
+        self._cost_actual = data[11]
 
     @property
     def year(self):
@@ -64,4 +70,24 @@ class Entry:
 
     def __str__(self):
         return f"Entry({self._id}, {self._year}, {self._budget_type}, {self._organization}, {self._command_name}, {self._pillar_name}, {self._district_name}, {self._unit_name}, {self._feature_category}, {self._cost_estimate}, {self._long_name}, {self._cost_actual})"
+
+class Dataset:
+    """
+    Dataset composed of all the entries
+
+    Columns in the dataset:
+    >>> print(BUDGET_2020[0])
+    ['_id', 'Fiscal_Year', 'Budget_Type', 'Organization_Entity', 'Command_Name', 'Pillar_Name', 'District_Name', 'Unit_Name', 'Feature_Category', 'Cost_Element', 'Cost_Element_Long_Name', 'Amount']
+
+    >>> print(BUDGET_2020[1])
+    ['1', '2020', 'Approved Budget', '1 - Toronto Police Service', 'Centralized Service Charges', 'Centralized Service Charges', 'Centralized Service Charges', 'CCC - Central Paid Duty', 'Revenues', '7652', 'PAID DUTY', '-1500']
+
+    >>> my_dataset = Dataset([Entry(BUDGET_2020[1]), Entry(BUDGET_2020[2]), Entry(BUDGET_2020[3]), Entry(BUDGET_2020[4])])
+    >>> print(my_dataset._data[2])
+    Entry(3, 2020, Approved Budget, 1 - Toronto Police Service, Centralized Service Charges, Centralized Service Charges, Centralized Service Charges, CCC - Central Paid Duty, Revenues, 8503, PAY DUTY EQUIPMENT RENTAL, -1450000)
+    """
+    _data : List[Entry]
+
+    def __init__(self, data: List[Entry]):
+        self._data = data
 
--- data_loader.py
+++ data_loader.py
@@ -7,11 +7,14 @@ DATA_DIRECTORY = "../toronto-police-spending/data/"
 def load_data(data):
     """Load data from a CSV file."""
     data_path = os.path.join(DATA_DIRECTORY, data)
-    with open(data_path, "r") as file:
-        reader = csv.reader(file)
-        header = next(reader)
-        rows = [row for row in reader]
-    return header, rows
+    dataset = []
+
+    with open(data_path, newline= '') as file:
+        reader = csv.reader(file, delimiter=',')
+        for row in reader:
+            dataset.append(row)
+
+    return dataset
 
 
 BUDGET_2020 = load_data('TPS_Budget_2020.csv')
@@ -21,4 +24,5 @@ BUDGET_2023 = load_data('TPS_Budget_2023.csv')
 BUDGET_2024 = load_data('TPS_Budget_2024.csv')
 BUDGET_2025 = load_data('TPS_Budget_2025.csv')
 
-print(BUDGET_2020)
+# print(BUDGET_2020[0])
+# print(BUDGET_2020[4])
